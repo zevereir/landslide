b71f74074256b907b0fac899c62668372302c53e
zevereir <sieben-freddy@hotmail.com>
2021-01-10 15:23:17 +0100

    Update pinacs_script.py

--- thesis_sieben_bocklandt/code/prototyping/pinacs_script.py
+++ thesis_sieben_bocklandt/code/prototyping/pinacs_script.py
@@ -8,11 +8,15 @@ from thesis_sieben_bocklandt.code.prototyping.RA2archetype import RA2archetype
 from thesis_sieben_bocklandt.code.prototyping.archetypes2slides import archetypes2slides
 from thesis_sieben_bocklandt.code.prototyping.work_with_scores import work_with_scores
 from thesis_sieben_bocklandt.code.prototyping.ppt_pdf_similarity import ppt_pdf_similarity
-
+import argparse
 def main():
-    args = sys.argv[1:]
-    source = args[0] #bvb landslide_data//1_data
-    name_output=args[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data",default=None)
+    parser.add_argument("--exp-name",default="baseline")
+    args = parser.parse_args()
+
+    source = args.data.replace("/","\\") #bvb landslide_data//1_data
+    name_output=args.exp_name
     if source.endswith("\\"):
         source=source[:-1]
     data=source[source.rfind("\\")+1:].replace("_data","")
