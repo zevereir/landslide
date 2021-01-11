6db0f91e00e802b395525cff4e9bf92d3c2d1c51
zevereir <sieben-freddy@hotmail.com>
2021-01-11 10:27:28 +0100

    learned archetypes

--- thesis_sieben_bocklandt/code/prototyping/Testing/test.py
+++ thesis_sieben_bocklandt/code/prototyping/Testing/test.py
@@ -1,4 +1,51 @@
-from thesis_sieben_bocklandt.code.prototyping.data_abstraction import data_abstraction
-data_abstraction(1,924,[1,27,28,36,54,58,64,75,105,112,126,138,154,155,162,185,193,201,203,215,217,223,251,267,272,277,287,292,304,310,311,317,327,332,353,355,362,365,391,408,
- 426,442,465,467,487,503,510,532,537,538,551,552,573,599,600,614,618,634,644,653,673,689,696,703,707,734,737,779,786,788,801,810,812,837,839,846,847,868,896,898,901,904,908]
-)
+# from thesis_sieben_bocklandt.code.prototyping.pdf2xml import pdf2xml
+# from thesis_sieben_bocklandt.code.prototyping.xml_preparse2tree import preparse_xml
+# from thesis_sieben_bocklandt.code.prototyping.tree2features import tree2features
+# from thesis_sieben_bocklandt.code.prototyping.tree2RA import tree2RA
+# import xml.etree.ElementTree as ET
+# pdf_file = "D:\\Thesis\\landslide\\possible_slides.pdf"
+# output_directory = "D:\\Thesis\\landslide\\possible_slides_data"
+# testing=False
+# print("-->PDF2XML")
+# xml_file = pdf2xml(pdf_file, output_directory)
+# print("-->preparse_xml")
+# xml_tree = preparse_xml(xml_file, output_directory)
+# # print("-->xml2image")
+# print("-->tree2features")
+# feature_tree = tree2features(xml_tree, xml_file, output_directory, True)
+# # print("-->xml2image")
+# print("-->tree2RA")
+# powerpoint, tree_with_indexes, one_background = tree2RA(feature_tree, xml_file)
+# for i in powerpoint.pages:
+#  print(i.RA)
+from thesis_sieben_bocklandt.code.prototyping.RA2archetype import optimal_substitution
+file1 = open('D:\\Thesis\\landslide\\sets.txt', 'r')
+Lines = file1.readlines()
+print(len(Lines))
+# for i in Lines:
+#  print(i)
+archetypes=[6,6,6,18,54,72,2,6,2,1]
+indices=   [2,2,2,3 ,4 ,5 ,1,3,2,1]
+finalized=[]
+for i in range(0,len(archetypes)):
+ result=[]
+ needed=Lines[sum(archetypes[0:i]):sum(archetypes[0:i])+archetypes[i]]
+ for line in needed:
+   already_in=False
+   repr = set([x.strip().replace("'","") for x in line[1:-2].split(",")])
+   for al in result:
+     dis,sol,map=optimal_substitution(frozenset(repr),frozenset(al),indices[i],indices[i])
+     if sol:
+        already_in=True
+        break
+   if not already_in:
+    result.append(repr)
+ finalized.append(result)
+
+#final results: [2,5,6,10,41,33,2,2,2,1]
+for i in finalized:
+ print(i)
+
+
+ #print(set(len(line[1:-1].split(","))))
+
