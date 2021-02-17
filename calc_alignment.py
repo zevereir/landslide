1cdac6f97732df2eb54cada151d8980ae1266365
zevereir <sieben-freddy@hotmail.com>
2021-02-17 11:48:42 +0100

    rules in alignment

--- calc_alignment.py
+++ calc_alignment.py
@@ -1,4 +1,69 @@
 import json
+
+def check_rules(alignment,elements,repr, element):
+    if len(elements)==0 or alignment=="TITLE":
+        return True
+    else:
+        if alignment in ["LEFTSUBTITLE","MIDDLESUBTITLE","LEFTCONTENT","MIDDLECONTENT"]:
+            for ele in elements:
+                if ele[1]=="RIGHTCONTENT" or ele[1]=="RIGHTSUBTITLE":
+                    for rel in repr:
+                        if "("+str(element)+","+str(ele[0])+")" in rel:
+                            if "-x" in rel:
+                                if rel[:rel.find("-")]  not in ["b","m","o","s","eq"]:
+                                    print("WRONGG")
+                                    return False
+                        elif "("+str(ele[0])+","+str(element)+")" in rel:
+                            if "-x" in rel:
+                                if rel[:rel.find("-")]  not in ["d","f","eq"]:
+                                    print("WRONGG")
+                                    return False
+        if alignment in ["RIGHTSUBTITLE,MIDDLESUBTITLE","RIGHTCONTENT","MIDDLECONTENT"]:
+            for ele in elements:
+                if ele[1]=="LEFTCONTENT" or ele[1]=="LEFTSUBTITLE":
+                    for rel in repr:
+                        if "("+str(element)+","+str(ele[0])+")" in rel:
+                            if "-x" in rel:
+                                if rel[:rel.find("-")]  in ["b","m","o","s"]:
+                                    print("WRONGG")
+                                    return False
+                        elif "("+str(ele[0])+","+str(element)+")" in rel:
+                            if "-x" in rel:
+                                if rel[:rel.find("-")]  in ["d","f"]:
+                                    print("WRONGG")
+                                    return False
+
+        if "SUBTITLE" in alignment:
+            for ele in elements:
+                if "CONTENT" in ele[1]:
+                    for rel in repr:
+                        if "("+str(element)+","+str(ele[0])+")" in rel:
+                            if "-y" in rel:
+                                if rel[:rel.find("-")]  in ["b","m","o","s"]:
+                                    print("WRONGG")
+                                    return False
+                        elif "("+str(ele[0])+","+str(element)+")" in rel:
+                            if "-y" in rel:
+                                if rel[:rel.find("-")]  in ["d","f"]:
+                                    print("WRONGG")
+                                    return False
+        if "CONTENT" in alignment:
+            for ele in elements:
+                if "SUBTITLE" in ele[1]:
+                    for rel in repr:
+                        if "("+str(element)+","+str(ele[0])+")" in rel:
+                            if "-y" in rel:
+                                if rel[:rel.find("-")]  not in ["b","m","o","s","eq"]:
+                                    print("WRONGG")
+                                    return False
+                        elif "("+str(ele[0])+","+str(element)+")" in rel:
+                            if "-y" in rel:
+                                if rel[:rel.find("-")]  not in ["d","f","eq"]:
+                                    print("WRONGG")
+                                    return False
+        return True
+
+
 with open("D://Thesis//landslide//data//Annotations//alignments.json") as ali:
     alignments=json.load(ali)
 with open("D://Thesis//landslide//data//Annotations//results//annotated_data//results//greedy_all_baseline_0_80000_False.json") as fp:
@@ -27,14 +92,19 @@ with open("D://Thesis//landslide//data//Annotations//results//annotated_data//re
             print("Slide = ",slide["New slide"])
             print("Archetype = ",slide["Archetype representation"])
             print("MAPPINGS")
+            current_mappings=set()
+            new_slide=slide["New slide"]
+            arch_repr=slide["Archetype representation"]
+            slide_repr={x for x in new_slide[11:-2].split(", ")}
+            print(slide_repr)
             for element in range(0,len(slide_roles)):
                 if str(element) in mapping:
                     if str(-mapping[str(element)]) in role_mapping:
                         print(slide_roles[element] +"--> "+role_mapping[str(-mapping[str(element)])].upper())
-                        if slide_roles[element] in alignments[str(arch)][role_mapping[str(-mapping[str(element)])].upper()]:
+                        if slide_roles[element] in alignments[str(arch)][role_mapping[str(-mapping[str(element)])].upper()] and check_rules(role_mapping[str(-mapping[str(element)])].upper(),current_mappings,slide_repr,element):
                             score+=1
-            new_slide=slide["New slide"]
-            arch_repr=slide["Archetype representation"]
+                        current_mappings.add((element,role_mapping[str(-mapping[str(element)])].upper()))
+
             if "background" in new_slide and "background" in arch_repr:
                 ind1=new_slide[new_slide.find("background")+11:new_slide.find("background")+12]
                 ind2=arch_repr[arch_repr.find("background")+11:arch_repr.find("background")+12]
